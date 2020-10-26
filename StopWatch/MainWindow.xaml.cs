edb0d952fd5b0a16690fe5bb05be3b7e131a214a
frost22a <frost22a@gmail.com>
2020-10-26 14:53:03 +0100

    up!

--- StopWatch/MainWindow.xaml.cs
+++ StopWatch/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace StopWatch
 {
@@ -20,9 +22,35 @@ namespace StopWatch
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        Stopwatch stopWatch = new Stopwatch();
+        DispatcherTimer dispatcherTimer = new DispatcherTimer();
+        TimeSpan timeSpan;
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent();                         
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            stopWatch.Start();
+            timeSpan = stopWatch.Elapsed;
+            stopWatchView.Text = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds, timeSpan.Milliseconds / 10);
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            if (stopWatch.IsRunning)
+            {
+                stopWatch.Stop();
+            }
+            
+        }
+
+        private void btnStart_Copy1_Click(object sender, RoutedEventArgs e)
+        {
+            stopWatch.Reset();
         }
     }
 }
